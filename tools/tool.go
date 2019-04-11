da071b58f3e186fbc81f0e11eb395852f1a218f9
joe <joewttx@163.com>
2019-04-11 21:45:17 +0800

    用户注册

--- tools/tool.go
+++ tools/tool.go
@@ -1,6 +1,11 @@
 package tools
 
-import "strconv"
+import (
+	"crypto/md5"
+	"io"
+	"strconv"
+	"regexp"
+)
 
 func StringToInt32(s string) (int32, error) {
 	id , err := strconv.ParseInt(s,10,32)
@@ -9,5 +14,25 @@ func StringToInt32(s string) (int32, error) {
 		return -1, err
 	}
 	return id64, nil
+}
+
+
+
+func CheckEmail(email string) (b bool) {
+	if m, _ := regexp.MatchString("^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+(.[a-zA-Z0-9_-])+", email); !m {
+		return false
+	}
+	return true
+}
+
+
+func MD5(s string)string {
+	h := md5.New()
+	_, _ = io.WriteString(h, s)
+	var x string
+	for _, v := range h.Sum(nil) {
+		x += strconv.FormatInt(int64(v), 16)
+	}
+	return x
 }
 
