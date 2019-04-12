f6c324db5204b0c87d821324a56632b1f6bbe143
joe <joewttx@163.com>
2019-04-12 15:39:28 +0800

    登录功能完善

--- routers/commentsRouter_controllers.go
+++ routers/commentsRouter_controllers.go
@@ -171,6 +171,15 @@ func init() {
 
     beego.GlobalControllerRouter["github.com/yinrenxin/hgoj/controllers:UserController"] = append(beego.GlobalControllerRouter["github.com/yinrenxin/hgoj/controllers:UserController"],
         beego.ControllerComments{
+            Method: "Login",
+            Router: `/login`,
+            AllowHTTPMethods: []string{"post"},
+            MethodParams: param.Make(),
+            Filters: nil,
+            Params: nil})
+
+    beego.GlobalControllerRouter["github.com/yinrenxin/hgoj/controllers:UserController"] = append(beego.GlobalControllerRouter["github.com/yinrenxin/hgoj/controllers:UserController"],
+        beego.ControllerComments{
             Method: "Profile",
             Router: `/profile/:uid`,
             AllowHTTPMethods: []string{"get"},
