452a00b1dac2507d372a4a657371e82086e03377
joe <joewttx@163.com>
2019-05-06 22:00:47 +0800

    添加文章&文章列表

--- controllers/blog.go
+++ controllers/blog.go
@@ -1,5 +1,9 @@
 package controllers
 
+import (
+	"github.com/astaxie/beego/logs"
+	"github.com/yinrenxin/hgoj/models"
+)
 
 type BlogController struct {
 	BaseController
@@ -12,19 +16,36 @@ func (this *BlogController) BlogIndex() {
 }
 
 
-// @router /article/add [get]
+// @router /admin/art/add [get]
 func (this *BlogController) BlogAddGet() {
-	this.TplName = "admin/addContest.html"
+	this.TplName = "admin/addArt.html"
 }
 
 
-// @router /article/add [post]
-func (this *BlogController) ContestAdd() {
-	this.TplName = "admin/addContest.html"
+// @router /admin/art/add [post]
+func (this *BlogController) BlogAddPost() {
+	if !this.IsAdmin {
+		this.Abort("401")
+	}
+	title := this.GetMushString("title", "标题不能为空")
+	content := this.GetMushString("content", "内容不能为空")
+
+	_, err := models.AddArt(this.User.UserId,title,content)
+	if err != nil {
+		this.JsonErr("文章添加错误",10000,"")
+	}
+
+	this.JsonOK("文章添加成功","/admin/art/list")
 }
 
 
-// @router /article/list [post]
-func (this *BlogController) ContestList() {
-	this.TplName = "admin/listContest.html"
+
+// @router /admin/art/list [get]
+func (this *BlogController) BlogList() {
+	Art, err := models.QueryAllArt()
+	if err != nil {
+		logs.Error("文章查询错误",err)
+	}
+	this.Data["art"] = Art
+	this.TplName = "admin/listArt.html"
 }
--- models/news.go
+++ models/news.go
@@ -13,3 +13,41 @@ type News struct {
 	Importance		int8			`orm:"default(0)"`
 	Defunct			string			`orm:"type(char);size(1);default(N)"`
 }
+
+
+
+
+func AddArt(uid int32, title string, content string) (int64, error) {
+	var Art News
+	Art.UserId = uid
+	Art.Content = content
+	Art.Title = title
+	Art.Time = time.Now()
+	artId, err := DB.Insert(&Art)
+	if err != nil {
+		return artId, err
+	}
+	return artId, nil
+}
+
+/**
+var pro []*Problem
+	problem := new(Problem)
+	qs := DB.QueryTable(problem)
+	num, err := qs.OrderBy("-problem_id").All(&pro)
+	if err != nil {
+		return nil,num,err
+	}
+	return pro,num, nil
+ */
+
+func QueryAllArt() ([]*News, error) {
+	var art []*News
+	news := new(News)
+	qs := DB.QueryTable(news)
+	_, err := qs.OrderBy("-news_id").All(&art)
+	if err != nil {
+		return nil, err
+	}
+	return art, nil
+}
