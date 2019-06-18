eb20cbe673e8b0b2ebfb5c12daa176f5bd7a7309
joe <joewttx@163.com>
2019-06-18 17:49:12 +0800

    修改登录日志表字段

--- models/loginlog.go
+++ models/loginlog.go
@@ -1,15 +1,14 @@
 package models
 
 import (
-	_ "github.com/astaxie/beego/orm"
 	"time"
+
+	_ "github.com/astaxie/beego/orm"
 )
 
-
-
 type LoginLog struct {
-	UserId		int32		`orm:"pk"`
-	Password 	string 		`orm:"size(32);null"`
-	IP			string  	`orm:"size(46);null"`
-	Time		time.Time	`orm:"auto_now_add;type(date);null"`
+	UserId   int32     `orm:"pk"`
+	Password string    `orm:"size(32);null"`
+	Ip       string    `orm:"size(46);null"`
+	Time     time.Time `orm:"auto_now_add;type(date);null"`
 }
