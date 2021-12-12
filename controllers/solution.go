28640371e9bf2f37d01c698a6239df00f34d4fa7
joe <joewttx@163.com>
2021-12-12 15:50:06 +0800

    fix: 🐛fix db rollback

--- controllers/solution.go
+++ controllers/solution.go
@@ -1,11 +1,12 @@
 package controllers
 
 import (
+	"time"
+
 	"github.com/beego/beego/v2/adapter/logs"
 	"github.com/yinrenxin/hgoj/models"
 	"github.com/yinrenxin/hgoj/syserror"
 	"github.com/yinrenxin/hgoj/tools"
-	"time"
 )
 
 type SolutionController struct {
@@ -26,37 +27,37 @@ func (this *SolutionController) Submit() {
 	ip := tools.SplitIP(addr)
 	uid := this.User.UserId
 
-
 	if conId != 0 {
 		con, err := models.QueryContestByConId(conId)
 		if err != nil {
-			this.JsonErr("系统错误", 9120,"")
+			logs.Info(err)
+			this.JsonErr("比赛信息不存在", 9120, "")
 		}
 		t := time.Now().Sub(con.EndTime).Seconds()
 		if t > 0 {
-			this.JsonErr("比赛已结束",9121,"")
+			this.JsonErr("比赛已结束", 9121, "")
 		}
 	}
 
 	_, err := models.AddSolution(proId, source, uid, code_length, lang, conId, ip)
 	if err != nil {
-		this.JsonErr("保存代码错误", syserror.SAVE_CODE_ERR, "problem")
+		logs.Error("add solution err:", err)
+		this.JsonErr("保存代码错误, 请稍后再试", syserror.SAVE_CODE_ERR, "problem")
 	}
-	this.JsonOK("提交成功","/status")
+	this.JsonOK("提交成功", "/status")
 }
-
 
 // @router /status/:page [get]
 func (this *SolutionController) StatusPage() {
 	page := this.Ctx.Input.Param(":page")
 	pageNo, _ := tools.StringToInt32(page)
 	pageNo = pageNo - 1
-	start := int(pageNo)*pageSize
-	data,RESULT,_,totalNum,err := models.QueryPageSolution(start,pageSize)
+	start := int(pageNo) * pageSize
+	data, RESULT, _, totalNum, err := models.QueryPageSolution(start, pageSize)
 	if err != nil {
 		logs.Error(err)
 	}
-	isPage, pagePrev, pageNext := PageCal(totalNum,pageNo,pageSize)
+	isPage, pagePrev, pageNext := PageCal(totalNum, pageNo, pageSize)
 
 	this.Data["isPage"] = isPage
 	this.Data["pagePrev"] = pagePrev
